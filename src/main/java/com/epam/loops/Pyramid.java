95112c6be3ab6229eb6e2a3cc333af23b31aeaca
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2022-12-08 18:30:38 +0200

    loops task completed

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,18 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            // add prefix:
+            for (int j = cathetusLength; j >= 1; j--) {
+                if (j > i) System.out.print(' ');
+                else System.out.print(j);
+            }
+            // add appendix:
+            for (int k = 2; k <= cathetusLength; k++) {
+                if (k <= i) System.out.print(k);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
