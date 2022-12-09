b1fd00016cc28d07b2330f896e80d8af4657272b
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2022-12-09 23:29:08 +0200

    algorithms task completed

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        return null;
+        return new String[]{"Winter", "Spring", "Summer", "Autumn"};
     }
 
     /**
@@ -23,7 +23,11 @@ public class ArrayTasks {
      * length = 1  -> [1] length = 3  -> [1, 2, 3] length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-        return null;
+        int[] res = new int[length];
+        for (int i = 1; i <= length; i++) {
+            res[i-1] = i;
+        }
+        return res;
     }
 
     /**
@@ -34,7 +38,9 @@ public class ArrayTasks {
      * arr = [1, 3, 5]   -> sum = 9 arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-        return 0;
+        int res = 0;
+        for (int i: arr) res += i;
+        return res;
     }
 
     /**
@@ -46,7 +52,10 @@ public class ArrayTasks {
      * arr = [99, -7, 102], number = -7    ->   2 arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-        return 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) return i;
+        }
+        return -1;
     }
 
     /**
@@ -58,7 +67,11 @@ public class ArrayTasks {
      * "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-        return null;
+        String[] res = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[arr.length - 1 - i];
+        }
+        return res;
     }
 
     /**
@@ -70,7 +83,68 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        return null;
+        // define the rea array size:
+        int k = 0;
+        for (int i : arr) {
+            if (i > 0) k++;
+        }
+        int[] res = new int[k];
+
+        // fill res array by positive numbers:
+        k = 0;
+        for (int j : arr) {
+            if (j > 0) {
+                res[k] = j;
+                k++;
+            }
+        }
+        return res;
+    }
+
+    public static int[] sortArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) continue;
+
+            int item = arr[i];
+            int j = i;
+            while (j > 0) {
+                int index_prev = j - 1;
+                int item_prev = arr[index_prev];
+
+                if (item < item_prev) {
+                    arr[j] = item_prev;
+                    arr[index_prev] = item;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static int[][] sortArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) continue;
+
+            int arr_len = arr[i].length;
+            int index_curr = i;
+
+            while (index_curr > 0) {
+                int index_prev = index_curr - 1;
+                int item_prev = arr[index_prev].length;
+                int[] curr_arr = arr[index_curr];
+
+                if (arr_len < item_prev) {
+                    arr[index_curr] = arr[index_prev];
+                    arr[index_prev] = curr_arr;
+                    index_curr--;
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
     }
 
     /**
@@ -82,8 +156,11 @@ public class ArrayTasks {
      * <p>
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]] arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    public int[][] sortRaggedArray(int[][] arr) {
-        return null;
+    public static int[][] sortRaggedArray(int[][] arr) {
+        arr = sortArray(arr);
+        for (int[] i_arr: arr) {
+            sortArray(i_arr);
+        }
+        return arr;
     }
-
 }
