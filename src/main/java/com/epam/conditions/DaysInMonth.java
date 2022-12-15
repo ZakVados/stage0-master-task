30389a4bf322500807cdf793e03b64551713b686
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2022-12-15 23:26:55 +0200

    completed condition task

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,45 @@
 package com.epam.conditions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class DaysInMonth {
 
-    public void printDays(int year, int month) {
+    public static List<Integer> uniqueNotLapYears = Arrays.asList(1700, 1800, 1900, 2100, 2200, 2300, 2500, 2600);
 
+    public static HashMap daysInMonth = new HashMap<Integer, Integer>() {{
+        put(1, 31);
+        put(2, 28);
+        put(3, 31);
+        put(4, 30);
+        put(5, 31);
+        put(6, 30);
+        put(7, 31);
+        put(8, 31);
+        put(9, 30);
+        put(10, 31);
+        put(11, 30);
+        put(12, 31);
+    }};
+
+    public boolean isLeapYear(int year) {
+        if (uniqueNotLapYears.contains(year)) return false;
+        return ((year % 4) == 0);
     }
 
+    public void printDays(int year, int month) {
+        // sanity check:
+        if ((year < 0) || ( month < 1 || month > 12 )) {
+            System.out.println("invalid date");
+            return;
+        }
+
+        if (isLeapYear(year) && (month == 2)) {
+            System.out.println(29);
+        } else {
+            System.out.println(daysInMonth.get(month));
+        }
+    }
 }
--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -1,8 +1,19 @@
 package com.epam.conditions;
 
+import java.util.Arrays;
+
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        // Треугольник можно построить в том случае, если сумма длин двух любых его сторон больше длины третьей стороны.
+        double[] sides = new double[]{firstSide, secondSide, thirdSide};
+        Arrays.sort(sides);
+
+        if ((sides[0] + sides[1]) > sides[2]) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
 
     }
 
